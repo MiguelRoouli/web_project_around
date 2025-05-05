0105a46daf3d0061856d01f39202c678a85ac1ea
MiguelRoouli <miguelroouli@gmail.com>
2025-05-05 17:14:31 -0600

    ajustes js, required form

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,11 @@
-let buttonEdit = document.querySelector(".main__button_edit");
-let popup = document.querySelector(".popup");
-let buttonClose = document.querySelector(".popup__button_close");
-let form = document.querySelector(".popup__container");
-let inName = document.querySelector(".main__paragraph_name");
-let inAbout = document.querySelector(".main__paragraph_about");
-let inputName = document.querySelector(".popup__input_name");
-let inputAbout = document.querySelector(".popup__input_about");
+const buttonEdit = document.querySelector(".main__button_edit");
+const popup = document.querySelector(".popup");
+const buttonClose = document.querySelector(".popup__button_close");
+const form = document.querySelector(".popup__container");
+const inName = document.querySelector(".main__paragraph_name");
+const inAbout = document.querySelector(".main__paragraph_about");
+const inputName = document.querySelector(".popup__input_name");
+const inputAbout = document.querySelector(".popup__input_about");
 
 function openEdit() {
   inputName.value = inName.textContent;
@@ -15,3 +15,12 @@ function openEdit() {
 
 buttonEdit.addEventListener("click", openEdit);
 buttonClose.addEventListener("click", openEdit);
+
+function saveChange(evt) {
+  evt.preventDefault();
+  inName.textContent = inputName.value;
+  inAbout.textContent = inputAbout.value;
+  openEdit();
+}
+
+form.addEventListener("submit", saveChange);
